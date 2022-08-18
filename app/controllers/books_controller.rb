81a476721afb1e478c373de5e200c4d6c29af98a
natsumi22222222 <110472548+natsumi22222222@users.noreply.github.com>
2022-08-18 11:30:26 +0000

    [Update]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,17 +5,17 @@ class BooksController < ApplicationController
 
   def index
     @books= Book.all
-    @book= Book.new
   end
 
   def create
     @book= Book.new(book_params)
-    book.save
-    redirect_to '/books/:book_id'
+    if @book.save
+    redirect_to book_path(@Book.id)
+    end
   end
 
   def show
-    @book = Book.find(params[:book_id])
+    @book = Book.find(params[:id])
   end
 
   def edit
@@ -24,7 +24,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book_id).permit(:title,:body)
+    params.require(:id).permit(:title,:body)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 
  root 'homes#top'
  get '/books'=>'books#index'
- get 'books/:book_id'=>'books#show'
  # post 'books'=>'books#create'
  resources :books
 
