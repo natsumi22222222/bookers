97dd6a3f50b6fc84217108af6d1ffd52ad64bd56
natsumi22222222 <110472548+natsumi22222222@users.noreply.github.com>
2022-08-21 06:40:30 +0000

    [Update]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,6 +8,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+     flash[:notice]= 'Book was successfully created.'
      redirect_to book_path(@book.id)
     else
      render :new
@@ -20,20 +21,21 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    flash[:notice]= 'Book was successfully updated.'
   end
-  
+
   def update
     book= Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book.id)
   end
-  
+
   def destroy
     book= Book.find(params[:id])
     book.destroy
     redirect_to books_path(book.id)
   end
-  
+
   private
 
   def book_params
